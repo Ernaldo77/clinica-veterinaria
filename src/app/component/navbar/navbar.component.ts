771d101130d0bc4a60fab3def633bb62a6122a5b
Jd <jdionisiokyo@gmail.com>
2025-04-30 12:24:55 -0300

    Erro no menu

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isScrolled = false;
+  menuAberto = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
+  }
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+
+  }
+
+  fecharMenu() {
+    this.menuAberto = false;
+  }
 }
